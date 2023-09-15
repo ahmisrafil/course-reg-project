ffa8e0e46d599bc94df2e45c24302d442391c942
ahmisrafil <ahmisrafil@yahoo.com>
2023-09-15 23:15:01 +0600

    Course registration project created with tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold text-center my-12'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
