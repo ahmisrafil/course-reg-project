72fedac9570e8dcbc697854a1c1e9f2457b395bc
ahmisrafil <ahmisrafil@yahoo.com>
2023-09-16 22:40:20 +0600

    cart information is made dinamic using hook

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,29 +3,27 @@
 import React, { useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
-const Cart = ({ selectedCourses }) => {
+const Cart = ({ selectedCourses, remainingCredit, totalCredit, totalPrice}) => {
+    console.log(remainingCredit);
 
     return (
         <div className='w-60 rounded-lg mx-4'>
-            <h3 className='text-lg font-bold text-[#2F80ED] my-4'>Credit Hour Remaining </h3>
+            <h3 className='text-base font-bold text-[#2F80ED] my-4'>Credit Hour Remaining {remainingCredit} hr</h3>
             <hr />
             <h3 className='text-xl font-bold my-4 '>Course Name</h3>
 
             <ol className='list-decimal'>
                 {selectedCourses?.map((course) =>
-                //     {
-                //     console.log(course.name);
-                // }
                 (
-                    <li>{course.name}</li>
+                    <li key={course.id}>{course.name}</li>
 
                 ))
                 }
             </ol>
             <hr />
-            <h3 className='text-base font-medium my-4 text-[#1C1B1B80]'>Total Credit Hour: </h3>
+            <h3 className='text-base font-medium my-4 text-[#1C1B1B80]'>Total Credit Hour: {totalCredit}</h3>
             <hr />
-            <h3 className='text-base font-medium my-4 text-[#1C1B1B80]'>Total Price:  </h3>
+            <h3 className='text-base font-medium my-4 text-[#1C1B1B80]'>Total Price: {totalPrice} USD </h3>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,12 @@ import Cart from '../Cart/Cart';
 const Home = () => {
     const [courses, setCourses] = useState([]);
     const [selectedCourses, setSelectedCourses] = useState([]);
-
+    const [remainingCredit, setRemainingCredit] = useState(20);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalCredit, setTotalCredit] = useState(0);
+    console.log(remainingCredit);
+    console.log(totalPrice);
+    console.log(totalCredit);
 
 
     useEffect(() => {
@@ -15,35 +20,64 @@ const Home = () => {
             .then(data => setCourses(data))
     }, []);
     const handleSelect = (course) => {
-        setSelectedCourses([...selectedCourses, course]);
+        const isExist = selectedCourses.find(courseName => courseName.id == course.id);
+        let credit = course.credit;
+        let price = course.price;
+
+        if (isExist) {
+            return alert("already selected the course");
+        }
+        else {
+            selectedCourses.forEach((course) => {
+                credit = credit + course.credit;
+                price = price + course.price;
+            });
+            if (credit > 20) {
+                return alert("You can not exceed your credit limit");
+            }
+            else {
+                setTotalCredit(credit);
+                setTotalPrice(price);
+                const creditRemaining = 20 - credit;
+                setRemainingCredit(creditRemaining);
+                setSelectedCourses([...selectedCourses, course]);
+            }
+
+
+        }
     }
 
     return (
         <div className='flex flex-row gap-4'>
             <div className='grid grid-cols-1 gap-2  lg:grid-cols-3'>
-            {
-                courses.map(course => (
-                    <div key={course.id} className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src={course.image} className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{course.name}</h2>
-                            <p className='text-base text-[#1C1B1B99]'>{course.details}</p>
-                            <div className='flex flex-row gap-2'>
-                                <p className='text-base text-[#1C1B1B99]'>$ Price: {course.price}</p>
+                {
+                    courses.map(course => (
+                        <div key={course.id} className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={course.image} className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{course.name}</h2>
+                                <p className='text-base text-[#1C1B1B99]'>{course.details}</p>
+                                <div className='flex flex-row gap-2'>
+                                    <p className='text-base text-[#1C1B1B99]'>$ Price: {course.price}</p>
 
-                                <p className='text-base text-[#1C1B1B99]'>Credit: {course.credit}hr</p>
-                            </div>
-                            <div className="card-actions">
-                                <button onClick={()=>handleSelect(course)} className='bg-[#2F80ED] px-24 py-2 rounded-md text-[#FFF] font-semibold text-lg'>Select</button>
+                                    <p className='text-base text-[#1C1B1B99]'>Credit: {course.credit}hr</p>
+                                </div>
+                                <div className="card-actions">
+                                    <button onClick={() => handleSelect(course)} className='bg-[#2F80ED] px-24 py-2 rounded-md text-[#FFF] font-semibold text-lg'>Select</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
             </div>
-            <Cart selectedCourses={selectedCourses}></Cart>
+            <Cart
+                selectedCourses={selectedCourses}
+                remainingCredit={remainingCredit}
+                totalCredit={totalCredit}
+                totalPrice={totalPrice}
+            ></Cart>
         </div>
     );
 };
